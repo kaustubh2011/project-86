20344e3ecab539e107a6175c86bb1ba795b49cd3
kaustubh2011 <81859002+kaustubh2011@users.noreply.github.com>
2021-07-04 16:58:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL('BirthdayImage.jpg',function(Img) {
+        block_obj=Img;
+         block_obj.scaleToWidth(700);  
+        block_obj.scaleToHeight(500); 
+         block_obj.set({
+             top:0,
+      left:0
+         }) ;
+         canvas.add(block_obj);
+          });	
+	
+}
+
+function playSound(){
+x.play();
+}
